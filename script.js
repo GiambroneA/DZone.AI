eb380f3d53fbbc2b60396bce67b7593a4fb1ab10
Angelo Giambrone <angelogiambrone7@gmail.com>
2025-05-04 18:33:53 -0400

    Add Red Wings page layout and scoped styling

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function goToTeam(team) {
-    window.location.href = team + ".html";
-  }
+document.getElementById("lions").addEventListener("click", () => {
+    window.location.href = "lions.html";
+  });
+  
+  document.getElementById("pistons").addEventListener("click", () => {
+    window.location.href = "pistons.html";
+  });
+  
+  document.getElementById("tigers").addEventListener("click", () => {
+    window.location.href = "tigers.html";
+  });
+  
+  document.getElementById("redwings").addEventListener("click", () => {
+    window.location.href = "redwings.html";
+  });
   
